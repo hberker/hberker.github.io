e08a5b8d728b0e001d33c79a0fa70e20d7e65f8c
Hosni Berker <hosni.berker@gmail.com>
2018-10-11 11:53:56 -0400

    typography module is added

--- gatsby-site/src/pages/index.js
+++ gatsby-site/src/pages/index.js
@@ -4,7 +4,7 @@ import { Link } from "gatsby"
 
 export default () => 
 (
-     <div style = {{color: 'purple', fontSize: '72px'}}>
+     <div style = {{margin: '3rem auto', maxWidth : 500, color: 'purple', fontSize: '72px'}}>
         <h1>Hello gatsby</h1>
         <Link to="/contact/">Contact</Link>
         <p> what a world</p>
