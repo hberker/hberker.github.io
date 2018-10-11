fe2a16a92e38ae050c98cab1712dd50bc58fcae6
Hosni Berker <hosni.berker@gmail.com>
2018-10-11 13:45:38 -0400

    added containers

--- gatsby-site/src/pages/index.js
+++ gatsby-site/src/pages/index.js
@@ -1,13 +1,28 @@
 import React from "react"
-import { Link } from "gatsby"
 
-
-export default () => 
-(
-     <div style = {{margin: '3rem auto', maxWidth : 500, color: 'purple', fontSize: '72px'}}>
-        <h1>Hello gatsby</h1>
-        <Link to="/contact/">Contact</Link>
-        <p> what a world</p>
-        <img src = "https://source.unsplash.com/random/400x200" alt=''></img>
-     </div>
+export default () => (
+  <div style = {{margin : '3rem auto', maxWidth : 600}}>
+    <h1>Richard Hamming on Luck</h1>
+    <div>
+      <p>
+        From Richard Hamming’s classic and must-read talk, “
+        <a href="http://www.cs.virginia.edu/~robins/YouAndYourResearch.html">
+          You and Your Research
+        </a>
+        ”.
+      </p>
+      <blockquote>
+        <p>
+          There is indeed an element of luck, and no, there isn’t. The prepared
+          mind sooner or later finds something important and does it. So yes, it
+          is luck.{" "}
+          <em>
+            The particular thing you do is luck, but that you do something is
+            not.
+          </em>
+        </p>
+      </blockquote>
+    </div>
+    <p>Posted April 09, 2011</p>
+  </div>
 )
