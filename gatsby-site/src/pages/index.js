dd5ed3784684e2982db70866e5769f907b240b4d
Hosni Berker <hosni.berker@gmail.com>
2018-10-15 13:12:55 -0400

    rearanged the project

--- gatsby-site/src/pages/index.js
+++ gatsby-site/src/pages/index.js
@@ -1,32 +1,23 @@
 import React from "react"
 import { Link } from "gatsby"
+import Layout from "./../components/Layout"
+import Post from "./../components/post"
+//import style from "/index.css"
 
 
-export default () => (
-  <div style = {{margin : '3rem auto', maxWidth : 600}}>
-    <h1>Richard Hamming on Luck</h1>
-    <div>
-      <p>
-        From Richard Hamming’s classic and must-read talk, “
-        <a href="http://www.cs.virginia.edu/~robins/YouAndYourResearch.html">
-          You and Your Research
-        </a>
-        ”.
-      </p>
-      <blockquote>
-        <p>
-          There is indeed an element of luck, and no, there isn’t. The prepared
-          mind sooner or later finds something important and does it. So yes, it
-          is luck.{" "}
-          <em>
-            The particular thing you do is luck, but that you do something is
-            not.
-          </em>
-        </p>
-      </blockquote>
-    </div>
-    <p>Posted April 09, 2011</p>
-    <p>Link to about Containers</p>
-    <Link to = "/about-css-modules/">Here</Link>
-  </div>
-)
+export default () => 
+(
+  <Layout> 
+      <ul>
+        <post>
+          <h1>First Post</h1>
+          <p>THis is an example of a first post</p>
+        </post>
+      </ul> 
+  </Layout> 
+
+
+
+
+
+)  
