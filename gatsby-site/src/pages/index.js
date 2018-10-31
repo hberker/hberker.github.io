8c2b0b1e550e335d7d1ed0e374edb00aef7f4395
Hosni Berker <hosni.berker@gmail.com>
2018-10-31 17:54:43 -0400

    changes a posts text

--- gatsby-site/src/pages/index.js
+++ gatsby-site/src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react"
 import { Link } from "gatsby"
 import Layout from "./../components/Layout"
 import styles from "./index.css"
-
+//Build Command: gatsby build --prefix-paths && gh-pages -d public -b master
 // #00A0C6;
 const Post = props => (
   <div className={props.name}>
@@ -43,7 +43,7 @@ export default () =>
           lang = "Java"
           excerpt ="In this project I recreated the classic game of snake in Java. 
           The game is simple: you control a 2-D snake made out of blocks, and direct
-           it across the screen as you try to consume apples. Ne"
+           it across the screen as you try to consume apples."
           link = "https://github.com/hberker/Snake"
         />
       </ul>
